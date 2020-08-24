84e9e43c814508c906535066109aa469ee53280c
Him4enko <eugenelive@ya.ru>
2020-08-24 15:26:57 +0300

    # Him4enko Build
    Используй бандл Windows от Ts.Saltan, у него в блоге он есть. (На гитхабе тоже:
    https://github.com/TsSaltan/jphp-windows-ext/releases/tag/2.2)

--- авыавы/src/app/forms/MainForm.php
+++ авыавы/src/app/forms/MainForm.php
@@ -2,9 +2,11 @@
 namespace app\forms; // НЕ ТРОГАТЬ
 
 
-use bundle\windows\Registry; // НЕ ТРОГАТЬ
-use bundle\windows\Windows; // НЕ ТРОГАТЬ
+use bundle\windows\Registry;
+use bundle\windows\Windows;
 use std, gui, framework, app; // НЕ ТРОГАТЬ
+use php\gui\event\UXEvent; 
+
 
 class MainForm extends AbstractForm
 {
@@ -13,27 +15,13 @@ class MainForm extends AbstractForm
      */
     function doShow(UXWindowEvent $e = null) // При открытии
     {
-        $nick_name = Registry::of('HKEY_CURRENT_USER\Software\www.gtasrv.ru\CR-MP\GenerationC')->read('player_name')->value; // НЕ ТРОГАТЬ
-        Element::setText($this->edit, $nick_name); // НЕ ТРОГАТЬ
+       $nickname = $this->ini->get('nickname', 'settings'); // НЕ ТРОГАТЬ
+        $this->edit->text = $nickname; // НЕ ТРОГАТЬ
         
-        $game_path = Registry::of('HKEY_CURRENT_USER\Software\www.gtasrv.ru\CR-MP\GenerationC')->read('game_path')->value; // НЕ ТРОГАТЬ
-        if($game_path) // НЕ ТРОГАТЬ
-        return Element::setText($this->label3, 'Путь указан'); // НЕ ТРОГАТЬ
-    }
-    /**
-     * @event buttonAlt.click-Left 
-     */
-    function doButtonAltClickLeft(UXMouseEvent $e = null) // Играть
-    {
-        $ip = '176.32.39.156'; // Ваш IP адрес
-        $port = '7777'; // Порт вашего IP адреса
-        
-        $game_path = Registry::of('HKEY_CURRENT_USER\Software\www.gtasrv.ru\CR-MP\GenerationC')->read('game_path')->value; // НЕ ТРОГАТЬ
-        $player_name = $this->edit->text; // НЕ ТРОГАТЬ
-        
-        Registry::of('HKEY_CURRENT_USER\Software\www.gtasrv.ru\CR-MP\GenerationC')->add('player_name', $player_name); // НЕ ТРОГАТЬ
-        execute("$game_path $ip:$port", false); // НЕ ТРОГАТЬ
-        app()->shutdown(); // НЕ ТРОГАТЬ
+        $game_path = $this->ini->get('game_path', 'settings'); // НЕ ТРОГАТЬ
+        if(empty($game_path)){
+          UXDialog::show('Путь не указан!'); // НЕ ТРОГАТЬ  
+        } 
     }
 
     /**
@@ -110,6 +98,31 @@ class MainForm extends AbstractForm
     function doImage3ClickLeft(UXMouseEvent $e = null)
     {    
         
+    }
+
+    /**
+     * @event button5.action 
+     */
+    function doButton5Action(UXEvent $e = null)
+    {    
+        $nickname = $this->edit->text;
+        $this->ini->set('nickname', $nickname, 'settings');
+    }
+
+    /**
+     * @event buttonAlt.action 
+     */
+    function doButtonAltAction(UXEvent $e = null)
+    {    
+             $ip = '176.32.39.156'; // Ваш IP адрес
+      $port = '7777'; // Порт вашего IP адреса
+        
+         $game_path = $this->ini->get('game_path', 'settings');
+         $nickname = $this->ini->get('nickname', 'settings');
+        
+        Registry::of('HKEY_CURRENT_USER\Software\www.gtasrv.ru\CR-MP\GenerationC')->add('player_name', $nickname); // НЕ ТРОГАТЬ
+       execute("$game_path $ip:$port", false); // НЕ ТРОГАТЬ
+    app()->shutdown(); // НЕ ТРОГАТЬ
     }
 
 }
